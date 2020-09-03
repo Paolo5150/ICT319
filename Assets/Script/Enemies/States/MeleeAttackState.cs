19b72150974c3937f66ae1107727829c164a7d4e
Paolo_X86 <p.ferri1986@gmail.com>
2020-09-03 20:44:16 +0800

    Melee damage

--- Assets/Script/Enemies/States/MeleeAttackState.cs
+++ Assets/Script/Enemies/States/MeleeAttackState.cs
@@ -8,6 +8,9 @@ public class MeleeAttackState : EnemyState
     public Vector3? playerLastKnownPosition;
     float stoppingDistance = 1.5f;
 
+    float meleeDamage = 5.0f;
+    float hitRate = 0.8f;
+    float timer = 0;
     Animator animator;
     public MeleeAttackState(Personality e) : base(e)
     {
@@ -35,9 +38,22 @@ public class MeleeAttackState : EnemyState
             playerLastKnownPosition = null;
             float toPlayerDist = (Player.Instance.transform.position - personalityObj.enemyObj.transform.position).magnitude;
             if (toPlayerDist < stoppingDistance) // Stopping distance, should be lower than view range
+            {
                 personalityObj.enemyObj.navigator.Stop();
+                if (timer <= 0)
+                {
+                    Player.Instance.OnGetShot(personalityObj.enemyObj.gameObject, meleeDamage);
+                    timer = hitRate;
+                }
+                else
+                    timer -= Time.deltaTime;
+
+            }
             else
+            {
                 personalityObj.enemyObj.navigator.Go(Player.Instance.transform.position);
+                timer = hitRate;
+            }
         }
         else
             playerLastKnownPosition = Player.Instance.transform.position;
@@ -49,6 +65,7 @@ public class MeleeAttackState : EnemyState
     public override void OnExit()
     {
         animator.SetBool("Attack", false);
+        timer = 0;
 
     }
 }
